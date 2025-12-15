c4bef04bb0f767f11b16e88aac7e71fd461dd201
JQC-2904 <jquinterocalabria@gmail.com>
2025-12-15 17:42:42 +0100

    Remove project files except README

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { VCenterSoap } from './nodes/VCenterSoap/VCenterSoap.node';
-import { VCenterSoapApi } from './credentials/VCenterSoapApi.credentials';
-export declare const nodes: (typeof VCenterSoap)[];
-export declare const credentials: (typeof VCenterSoapApi)[];
